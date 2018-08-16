9e8fa932e9d12686963fa45e51be55f1fb4a1bb9
Abraham V <abraham.varricatt@googlemail.com>
2018-08-15 22:29:47 -0400

    hope it builds

--- build.sh
+++ build.sh
@@ -4,3 +4,16 @@ echo 'hello world'
 python --version
 sass --version
 node --version
+
+npm install -g sass
+
+echo 'after install sass'
+sass --version
+echo 'done'
+
+echo 'install Nikola'
+pip install --upgrade "Nikola[extras]"
+
+echo 'build site'
+
+nikola build
